d62dc758d588ad61b7017a2565fcf895f8d1425d
Traversal2021 <82635403+Traversal2021@users.noreply.github.com>
2021-10-27 15:20:56 +0200

    Liu‘s final version

--- Assets/Script/SkyExposure.cs
+++ Assets/Script/SkyExposure.cs
@@ -4,78 +4,51 @@ using UnityEngine;
 
 public class SkyExposure : MonoBehaviour
 {
-     public Vector3 camPosition;
-     public float SkyExposurePercentage;
-     //public int numOfPixel;
-     //public int numOfSky;
-     public Camera cam;
-     public GameObject[] myBuilding;
+    public Camera cam;
+    public Vector3 mousePos;
+    public float SkyExposurePercentage;
 
-     
-     private float ScanTime;
-     private float NextScanTime;
-
-     Color skyColor= new Color(95f/255f,158f/255f,160f/255f);
-     Color buildingColor= new Color(255f/255f,248f/255f,220f/255f);
-
-    
     void Start(){
-        transform.position=camPosition;
-        transform.eulerAngles= new Vector3(-90,0,0);
-
         cam = GetComponent<Camera>();
-        cam.clearFlags = CameraClearFlags.SolidColor;
-
-        myBuilding= GameObject.FindGameObjectsWithTag("Building");
-
-        ScanTime=3.0f;
-
-
-        cam.backgroundColor = skyColor;
-
-        foreach (GameObject building in myBuilding)
-        {
-            building.GetComponent<Renderer>().material.SetColor("_Color",buildingColor);
-        }
-
     }
 
-
+//get the position of mouse click
     void Update(){
+        if(Input.GetMouseButtonDown(0)){ 
 
-
-        NextScanTime+=Time.deltaTime;
-        if(NextScanTime>=ScanTime){
-            NextScanTime=0;
-            StartCoroutine(CaptureScreenShot());
-        }
-    }
-
-    IEnumerator CaptureScreenShot(){
-
-
-
-        yield return new WaitForEndOfFrame();
-        int numOfPixel=0;
-        int numOfSky=0;
-
-        Texture2D m_texture=new Texture2D(Screen.width,Screen.height,TextureFormat.RGB24, false);
-        m_texture.ReadPixels(new Rect(0,0,Screen.width,Screen.height),0,0);
-        m_texture.Apply();
-        for (int a=0;a<=Screen.width;a++){
-            for (int b=0;b<=Screen.height;b++){
-                 numOfPixel++;
-                Color colorInShot = m_texture.GetPixel(a,b);
-                if(colorInShot.ToString()==skyColor.ToString()){
-                    numOfSky++;
-                }
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit raycast;
+            if(Physics.Raycast(ray, out raycast,float.MaxValue, 1<<LayerMask.NameToLayer("Ground"))){
+                mousePos=raycast.point;
+                StartCoroutine(CalculateSkyExposure());
             }
-        }
-
-        SkyExposurePercentage= numOfSky*100.0f/numOfPixel;
-        
+        }   
     }
 
 
+//calculate the sky exposure
+    IEnumerator CalculateSkyExposure(){
+       int counter_sky=0;
+       int counter_total=0;
+       int i,j;
+        for (i=30;i<=150;i++){
+            for(j=60;j>=-60;j--){
+                counter_total++;
+                Vector3 rayDirection=new Vector3(Mathf.Cos(i*Mathf.PI/180),Mathf.Sin(i*Mathf.PI/180)*Mathf.Cos(j*Mathf.PI/180),Mathf.Sin(i*Mathf.PI/180)*Mathf.Sin(j*Mathf.PI/180));
+                Ray ray=new Ray(mousePos,rayDirection);
+                RaycastHit hit;
+                if(Physics.Raycast(ray, out hit, float.MaxValue, 1<<LayerMask.NameToLayer("Default")))  
+                   {  
+                       counter_sky++;                        
+
+                   }  
+            }
+
+        }
+
+     SkyExposurePercentage=(1-counter_sky*1.0f/counter_total)*100;
+     yield return new WaitForSeconds(3f);
+        
+    }   
 
 }
