7e39c71bfa83367a9c2a48d50f82f673ec6e4573
pmleinan <permle@stud.ntnu.no>
2021-12-04 19:16:30 +0100

    Fix altitude

--- Assets/Script/SphereController.cs
+++ Assets/Script/SphereController.cs
@@ -21,6 +21,7 @@ public class SphereController : MonoBehaviour
     private float totalRays = 0;
     private float raysHit = 0;
     private int rotationPoints = 360;
+    private float landmarkHeight;
 
     public Text visIndexDisplay;
 
@@ -29,7 +30,11 @@ public class SphereController : MonoBehaviour
     {
         cameraPosition = GameObject.Find("Sphere").transform.position;
         originPosition = cameraPosition;
-        rayDirection = Vector3.forward; 
+        rayDirection = Vector3.forward;
+
+        Mesh mesh = landmark.GetComponent<MeshFilter>().mesh;
+        Bounds bounds = mesh.bounds;
+        landmarkHeight = bounds.size[1]; // Hight of landmark
     }
 
 
@@ -38,35 +43,37 @@ public class SphereController : MonoBehaviour
         RaycastHit hit;
 
         Visibiliyt_Index = 0;
-        for (int r = 0; r < 1; r++)
+        for (int r = 0; r < landmarkHeight; r = r + 10) // Check every 10 in hight of the landmark
         {
-            for (int j = 0; j < rotationPoints; j++)
+            for (int j = 0; j < rotationPoints; j++) // Rotate around the landmark
             {
-                for (float s = 0; s < 360; s += 1.0F)
+                for (float s = 0; s < 360; s += 1.0F) // Rotate around at each point
                 {
                     if (Physics.Raycast(transform.position, transform.TransformDirection(rayDirection), out hit, Mathf.Infinity))
                     {
-                        Debug.DrawRay(transform.position, transform.TransformDirection(rayDirection) * hit.distance, Color.yellow);
+                        //Debug.DrawRay(transform.position, transform.TransformDirection(rayDirection) * hit.distance, Color.yellow);
 
                         hitBuilding = hit.collider;
                         //hitBuilding.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
 
-                        totalRays++;
-                        raysHit++;
+                        totalRays++; // Count rays
+                        raysHit++; // Count rays that hits
                     }
                     else
                     {
-                        Debug.DrawRay(transform.position, transform.TransformDirection(rayDirection) * 1000, Color.white);
-                        totalRays++;
+                        //Debug.DrawRay(transform.position, transform.TransformDirection(rayDirection) * 1000, Color.white);
+                        totalRays++; // Count rays
                     }
-                    Vector3 test = new Vector3(0, s, 0);
-                    transform.rotation = Quaternion.Euler(test);
+                    Vector3 rotate = new Vector3(0, s, 0);
+                    transform.rotation = Quaternion.Euler(rotate);
                 }
-                Visibiliyt_Index = Mathf.Round(raysHit / totalRays * 100);
+                
                 transform.RotateAround(landmark.transform.position, Vector3.up, 1);
             }
+            transform.position = new Vector3(0, r, 0);
+            Debug.Log(transform.position);
         }
-
-        visIndexDisplay.text = ("Visibility index: " + Visibiliyt_Index);
+        Visibiliyt_Index = Mathf.Round(raysHit / totalRays * 100); // Calculate visibility index
+        visIndexDisplay.text = ("Visibility index: " + Visibiliyt_Index); // Return index to canvas
     }
 }
